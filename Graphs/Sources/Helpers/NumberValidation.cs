e7552f3dec463b0793a72c92ef0cdeb1281ebfec
Forichok <forichok@icloud.com>
2019-03-27 01:28:25 +0300

    Finally add validation

---
+++ Graphs/Sources/Helpers/NumberValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Graphs.Sources.Helpers
+{
+    class NumberValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int i;
+            if (!Int32.TryParse(value.ToString(), out i))
+            {
+                return new ValidationResult(false, "Not Num");
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
